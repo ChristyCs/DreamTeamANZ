85b28d903dfd0ef281d7bb09a56ea6d2d3f2635e
ChristyCs <christy.cartysquires@hotmail.co.nz>
2015-06-10 15:38:45 +1200

    Trying to make a data.js file to read the data

---
+++ data.js
@@ -0,0 +1,46 @@
+var data = function(){
+	 this.allData = [];
+     this.data2008 = [];
+     this.data2009 = [];
+     this.data2010 = [];
+     this.data2011 = [];
+     this.data2012 = [];
+     this.data2013 = [];
+}
+
+data.prototype = {
+	constructer : data,
+    readData : function(d){               
+                return {
+                    round: +d.Round,
+                    date: d.Date,
+                    time: d.Time,
+                    hometeam: d['Home Team'],
+                    score: d.Score,
+                    awayteam: d['Away Team'],
+                    venue: d.Venue                
+                 };
+            },
+    getAll : function(){
+    	return this.allData;
+    },
+    set2008 : function(list){
+    	this.data2008 = list;
+    },
+    set2009 : function(){
+    	return this.data2009;
+    },
+    set2010 : function(){
+    	return this.data2010;
+    },
+    set2011 : function(){
+    	return this.data2011;
+    },
+    set2012 : function(){
+    	return this.data2012;
+    },
+    set2013 : function(){
+    	return this.data2013;
+    }
+
+}
