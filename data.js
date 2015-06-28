cb553f76afc1f78659560e590782f820fa283a51
ChristyCs <christy.cartysquires@hotmail.co.nz>
2015-06-28 14:26:18 +1200

    Fixed a file reading bug

--- data.js
+++ data.js
@@ -25,22 +25,22 @@ data.prototype = {
     	return this.allData;
     },
     set2008 : function(list){
-    	this.data2008.push(list);
+    	this.data2008 = list;
     },
     set2009 : function(list){
-        this.data2009.push(list);
+        this.data2009 = list;
     },
     set2010 : function(list){
-        this.data2010.push(list);
+        this.data2010 = list;
     },
     set2011 : function(list){
-        this.data2011.push(list);
+        this.data2011 = list;
     },
     set2012 : function(list){
-        this.data2012.push(list);
+        this.data2012 = list;
     },
     set2013 : function(list){
-        this.data2013.push(list);
+        this.data2013 = list;
     },
 
 }
